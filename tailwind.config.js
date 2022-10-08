416dd6aed43dd3c994107e854c799c4e40904d6c
openwell <timileyinojo@yahoo.com>
2022-10-08 18:44:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+  content: ['*.{html,css}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          900: '#050923',
+        },
+      },
+      fontFamily: {
+        sans: ['SF-Regular', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+};
