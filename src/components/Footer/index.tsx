50d5a81fbe184dc444fc7a714b060cea6f4f83b0
openwell <38228398+openwell@users.noreply.github.com>
2024-04-19 16:23:03 +0100

    social links update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,12 +13,12 @@ export default function Footer() {
       <div className="px-4 py-12">
         {/* xl:gap-[140px] */}
         <div className="flex flex-wrap justify-between xl:flex-nowrap">
-          <div className='mb-10'>
+          <div className="mb-10">
             <Link href="/">
               <Image
                 src="/images/ndewo_by_betascore.png"
                 alt="beta-score logo"
-                className='w-[120px] xl:h-[71px] xl:w-[264px] '
+                className="w-[120px] xl:h-[71px] xl:w-[264px] "
                 width={264}
                 height={71}
               />
@@ -49,7 +49,7 @@ export default function Footer() {
                 </li>
               </ul>
             </div>
-            <div className='flex-1'>
+            <div className="flex-1">
               <h3 className="text-lg xl:text-xl leading-6 text-black xl:font-semibold">
                 Company
               </h3>
@@ -134,28 +134,40 @@ export default function Footer() {
               </h3>
               <ul role="list" className="mt-6 space-y-4">
                 <li>
-                  <Link
-                    href="/privacy-policy"
+                  <a
+                    target="_blank"
+                    href="https://twitter.com/getbetascore?s=21&t=UZn3090cyfwEqjLjR4CDIQ"
                     className="xl:text-xl leading-6 text-b-black-grey  hover:text-gray-500"
                   >
                     Twitter
-                  </Link>
+                  </a>
                 </li>
                 <li>
-                  <Link
-                    href="/terms-conditions"
+                  <a
+                    target="_blank"
+                    href="https://instagram.com/getbetascore?igshid=OGQ5ZDc2ODk2ZA=="
                     className="xl:text-xl leading-6 text-b-black-grey  hover:text-gray-500"
                   >
                     Instagram
-                  </Link>
+                  </a>
                 </li>
                 <li>
-                  <Link
-                    href="/terms-conditions"
+                  <a
+                    target="_blank"
+                    href="https://www.linkedin.com/company/betascore/"
                     className="xl:text-xl leading-6 text-b-black-grey  hover:text-gray-500"
                   >
                     LinkedIn
-                  </Link>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    target="_blank"
+                    href="https://www.facebook.com/share/Q2YPT3RNeNkubgyv/?mibextid=LQQJ4d"
+                    className="xl:text-xl leading-6 text-b-black-grey  hover:text-gray-500"
+                  >
+                    Facebook
+                  </a>
                 </li>
               </ul>
             </div>
@@ -164,7 +176,9 @@ export default function Footer() {
       </div>
       <div className="h-[1px] w-full bg-b-black-grey hidden xl:block" />
       <div className="flex flex-col lg:flex-row justify-between items-center py-8 px-4">
-        <p className="text-b-black-grey mb-6 text-sm xl:text-base">2023 Betascore. All rights reserved</p>
+        <p className="text-b-black-grey mb-6 text-sm xl:text-base">
+          2023 Betascore. All rights reserved
+        </p>
         <p className="text-b-black-grey max-w-[621px] text-center lg:text-right lg:text-sm xl:text-base hidden lg:block">
           Ndewo is a product of Betascore Finance. A company registered in the
           United Kingdom <br /> Registration no. 14536091 addressed at
@@ -174,10 +188,10 @@ export default function Footer() {
         </p>
         <p className="text-b-black-grey text-center text-sm lg:hidden">
           Ndewo is a product of Betascore Finance. A company registered in the
-          United Kingdom Registration no. 14536091 addressed at
-          Piccadilly Business Centre Unit C Aldow Enterprise Park
-          Blackett Street Manchester M12 6AE Betascore Finance is
-          regulated by FCA. Registration is under processing.
+          United Kingdom Registration no. 14536091 addressed at Piccadilly
+          Business Centre Unit C Aldow Enterprise Park Blackett Street
+          Manchester M12 6AE Betascore Finance is regulated by FCA. Registration
+          is under processing.
         </p>
       </div>
     </footer>
